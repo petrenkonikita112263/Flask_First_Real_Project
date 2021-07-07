ab9de468bc4db87e4797c8cd15b983440153aa9e
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-07 19:22:29 +0300

    Added template for home page, updated decorates and function to display home page.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
