8cbbc95ed3aaec9245ba944e66701d990d75ee15
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-07-09 11:40:57 +0300

    Changed models.py file.
    Created User class which is related to registration, last line it's not the column it's the relationship with products,
    that user has bought. The owner of the product displays in SalableGood model as the last column with id as foreign key to User.
    By default the user has 1000$ in his|her account.

--- market/models.py
+++ market/models.py
@@ -1,12 +1,22 @@
 from market import app_db
 
 
+class User(app_db.Model):
+    id = app_db.Column(app_db.Integer(), primary_key=True)
+    username = app_db.Column(app_db.String(length=30), nullable=False, unique=True)
+    user_email = app_db.Column(app_db.String(length=50), nullable=False, unique=True)
+    user_pd = app_db.Column(app_db.String(length=60), nullable=False)
+    budget = app_db.Column(app_db.Integer(), nullable=False, default=1000)
+    items = app_db.relationship("SalableGood", backref="owned_user", lazy=True)
+
+
 class SalableGood(app_db.Model):
     id = app_db.Column(app_db.Integer(), primary_key=True)
     name = app_db.Column(app_db.String(length=30), nullable=False, unique=True)
     price = app_db.Column(app_db.Integer(), nullable=False)
     barcode = app_db.Column(app_db.String(length=12), nullable=False, unique=True)
     description = app_db.Column(app_db.String(length=1024), nullable=False, unique=True)
+    owner = app_db.Column(app_db.Integer(), app_db.ForeignKey("user.id"))
 
     def __repr__(self):
         return f"SalableGood {self.name}"
